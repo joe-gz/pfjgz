553e141cd0140e75467bf58ba3ed268e2e0358ce
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2016-07-17 18:22:45 -0400

    adds data

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ var introButton = document.querySelector("#intro-button")
 
 var map = L.map('map').setView([38.9072, -77.0369], 12);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  maxZoom: 18,
-  id: 'joegz.0g5b9af9',
-  accessToken:'pk.eyJ1Ijoiam9lZ3oiLCJhIjoiY2lwdTR1czM1MDhiOGZ1bTJ0YmZyYmRjaiJ9.xvTMZMcfWMKG3ZLFahgJ9g'
-}).addTo(map);
+// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//   maxZoom: 18,
+//   id: 'joegz.0g5b9af9',
+//   accessToken:'pk.eyJ1Ijoiam9lZ3oiLCJhIjoiY2lwdTR1czM1MDhiOGZ1bTJ0YmZyYmRjaiJ9.xvTMZMcfWMKG3ZLFahgJ9g'
+// }).addTo(map);
 
 var sidebar = L.control.sidebar('sidebar', {
     position: 'left'
@@ -18,19 +18,6 @@ var sidebar = L.control.sidebar('sidebar', {
 map.addControl(sidebar);
 
 var photoDiv = '';
-
-// function circlestyle(feature) {
-//   return {
-//     radius: 8,
-//     fillColor: 'red',
-//     weight: 1,
-//     opacity: 1,
-//     color: 'white',
-//     // dashArray: '3',
-//     fillOpacity: 1,
-//     className: 'circleMarker'
-//   };
-// }
 
 var heartIcon = L.divIcon({className: 'heart-div-icon', iconSize: null});
 var foodIcon = L.divIcon({className: 'food-div-icon', iconSize: null});
@@ -57,8 +44,6 @@ pointMarkers = L.geoJson(points, {
       console.log(photoDiv);
       return photoDiv.innerHTML
     };
-    // point.bindPopup('<p><span class=spanText>What</span>: '+feature.properties.Description+'</p><p><span class=spanText>Where</span>: '+feature.properties.Location+'</p>'+'<p><span class=spanText>Date</span>: '+feature.properties.Date+'</p><p><span class=spanText>Time</span>: '+feature.properties.Time+'</p><div>'+showPhotos()+'</div>',
-    // {maxWidth: 250, minWidth: 200, maxHeight: 150, autoPan: true});
     point.on('click', function (){
       var sidebarDiv = document.querySelector("#sidebar");
       sidebarDiv.innerHTML = '<p><span class=spanText>What</span>: '+feature.properties.Description+'</p><p><span class=spanText>Where</span>: '+feature.properties.Location+'</p>'+'<p><span class=spanText>Date</span>: '+feature.properties.Date+'</p><p><span class=spanText>Time</span>: '+feature.properties.Time+'</p><div>'+showPhotos()+'</div>'
